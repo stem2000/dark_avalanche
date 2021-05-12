3a73d21af12b8616db434b48c4262c1ad51fea1f
stem <zaregenilsya@yandex.by>
2021-05-12 16:31:37 +0300

    Новая сцена

---
+++ Assets/Scripts/SceneInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneInfo{
+
+    private static GameObject prefabName;
+
+    public static GameObject Prefab
+    {
+        get
+        {
+            return prefabName;
+        }
+        set
+        {
+            prefabName = value;
+        }
+    }
+}
